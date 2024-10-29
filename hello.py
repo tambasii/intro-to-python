32dd19bd900ff59ba6540d932ae032124a7a9e66
tambasii <wenslastambasi@gmail.com>
2024-10-29 23:33:17 +0300

    Strip in a name to remove empty spaces

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 # Ask user for their name
 name = input("What's your name? ")
 
+#remove whitespace using strip()
+#name = name.strip()
+
 #Say hello to user, referring back to their name
 print("Hello, " + name)
